d7306423d69f64623c872f02ad50712c6ab2b52c
Uniliva Alves <uniliva@hotmail.com>
2018-12-18 09:58:05 -0200

    fist commit

---
+++ src/main/java/br/com/unidev/treinandomricroservicos/TreinandoMricroservicosApplication.java
@@ -0,0 +1,14 @@
+package br.com.unidev.treinandomricroservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinandoMricroservicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinandoMricroservicosApplication.class, args);
+	}
+
+}
+
