8d07d4be9e46e4c74a896423f99833e6edc984c3
Uniliva <Uniliva@UniDeskTop>
2019-01-05 20:46:09 -0200

    Criando paginação, com passagem de parametro na requisição

--- src/main/java/br/com/unidev/base/resource/CategoriaResource.java
+++ src/main/java/br/com/unidev/base/resource/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -30,31 +32,52 @@ public class CategoriaResource {
 
 	@GetMapping("")
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
-		List<CategoriaDTO> listaDTO = service.findAll().stream().map(cat -> new CategoriaDTO(cat)).collect(Collectors.toList());
+		List<CategoriaDTO> listaDTO = service.findAll().stream().map(cat -> new CategoriaDTO(cat))
+				.collect(Collectors.toList());
 		return ResponseEntity.ok().body(listaDTO);
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Categoria> findByFiltro(@PathVariable Integer id) {
 		return ResponseEntity.ok().body(service.find(id));
 	}
-	
+
 	@PostMapping("")
-	public ResponseEntity<Void> insert(@RequestBody Categoria categoria){
-		Categoria obj = service.insert(categoria);	
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		Categoria obj = service.insert(categoria);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();		
+		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@PutMapping("/{id}")
-	public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Categoria categoria ) {
-		service.update(categoria);	
-		return ResponseEntity.noContent().build();		
+	public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Categoria categoria) {
+		service.update(categoria);
+		return ResponseEntity.noContent().build();
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Integer id ) throws NotFoundException{
-		service.delete(id);	
-		return ResponseEntity.noContent().build();		
+	public ResponseEntity<Void> delete(@PathVariable Integer id) throws NotFoundException {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/paginas")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			// definindo que os valores viraão como paremetros da requicição ex
+			// /categoria/page?numero=1&numeroItens=4 ...
+			// o defaultValor e para definir um valor caso não seja passado por parametro
+			@RequestParam(value = "pagina", defaultValue = "0") Integer page,
+			@RequestParam(value = "numeroItens", defaultValue = "24") Integer itensPorPagina,
+			@RequestParam(value = "ordenadoPor", defaultValue = "nome") String OrdenadoPor,
+			@RequestParam(value = "direcao", defaultValue = "ASC") String direcaoOrdenação) {
+
+		Page<Categoria> PageCategoria = service.findPage(page, itensPorPagina, OrdenadoPor, direcaoOrdenação);
+
+		// convertendo uma Page de categoria para uma page de categoriaDTO objeto que
+		// vai ser serializado
+		// por page vir com o java ( não e necessario pegar um stream para usar o MAP
+		Page<CategoriaDTO> PageCategoriaDTO = PageCategoria.map(cat -> new CategoriaDTO(cat));
+
+		return ResponseEntity.ok().body(PageCategoriaDTO);
 	}
 }
--- src/main/java/br/com/unidev/base/services/CategoriaService.java
+++ src/main/java/br/com/unidev/base/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.unidev.base.domain.Categoria;
@@ -17,10 +20,10 @@ public class CategoriaService {
 	CategoriaRepository dao;
 
 	public Categoria find(Integer id) {
-		return dao.findById(id).orElseThrow(()-> new NotFoundException("Categoria não encontrado"));
+		return dao.findById(id).orElseThrow(() -> new NotFoundException("Categoria não encontrado"));
 	}
 
-	public Categoria insert(Categoria categoria) {		
+	public Categoria insert(Categoria categoria) {
 		categoria.setId(null);
 		return dao.save(categoria);
 	}
@@ -33,16 +36,30 @@ public class CategoriaService {
 	public void delete(Integer id) throws NotFoundException {
 		find(id);
 		try {
-		
-		dao.deleteById(id);
-		
-		}catch (DataIntegrityViolationException e) {
+
+			dao.deleteById(id);
+
+		} catch (DataIntegrityViolationException e) {
 			throw new BusinessException("Não é possivel apagar uma categoria a que tenha produtos");
 		}
 	}
 
 	public List<Categoria> findAll() {
 		return dao.findAll();
+	}
+
+	/**
+	 * 
+	 * @param page - Numero da pagina a ser retonada
+	 * @param itensPorPagina - quantidade de item que devem ser retornado
+	 * @param OrdenadoPor - item a ser usado para ordenar, [ex: id, nome ..]
+	 * @param direcaoOrdenação - Direção da ordenação se ASC ou DESC
+	 * @return
+	 */
+	public Page<Categoria> findPage(Integer page, Integer itensPorPagina, String OrdenadoPor, String direcaoOrdenação) {
+		PageRequest request = PageRequest.of(page, itensPorPagina, Direction.valueOf(direcaoOrdenação), OrdenadoPor);
+		return dao.findAll(request);
+
 	}
 
 }
